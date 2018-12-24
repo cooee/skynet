80f5ec8999ba155c5c8465c1c10d5d8b5b940064
YuchengMo <archermyc@gmail.com>
2018-12-24 17:21:01 +0800

    fix: 测试
    详细信息:
    
    # 提交类型字段
    # feat：新功能（feature）
    # fix： 修补bug
    # docs：文档（documentation）
    # refactor：重构（即不是新增功能，也不是修改bug的代码变动）
    # test：增加测试

--- examples/watchdog/main.lua
+++ examples/watchdog/main.lua
@@ -1,9 +1,9 @@
 local skynet = require "skynet"
 local max_client = 64
-
+require("LuaKit._load");
 skynet.start(function()
 
-	print("main start")
+	print("main start2")
 	-- skynet.error("Server start")
 	-- skynet.uniqueservice("protoloader")
 	local watchdog = skynet.newservice("watchdog")
@@ -18,6 +18,8 @@ skynet.start(function()
 	skynet.call(TableManager, "lua", "start", {
 		maxTableNum = 100,
 	})
+
+	dumpToFile("myc_test", {a = 2})
 
 	skynet.exit()
 	print("main exit")
